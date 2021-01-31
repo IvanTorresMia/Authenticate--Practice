4769c1c2a4fd536a85e731d99759c9946e18eb7e
Ivan Torres <ivantorresmia27@gmail.com>
2021-01-30 19:04:00 -0800

    Finished Home component

--- src/Components/Cards/CardList.jsx
+++ src/Components/Cards/CardList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Card from './Card';
 import { data } from '../../data'
-import Card from './Card';
+
 
 
 
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React from "react";
+import Terminal from "../Components/displays/Terminal";
+import CardList from "../Components/Cards/CardList";
 
 const Home = () => {
-
-
-    return (
-
-        <div>
-            Home
-        </div>
-    )
-}
+  return (
+    <div className="page" style={{ textAlign: "center" }}>
+      <p className="page-title">Simple OAuth with Node.js</p>
+      <p style={{ fontSize: 20 }}>
+        Passoport.js container support for over{" "}
+        <span style={{ color: "var(--primary-red)" }}>500</span>
+        Get started today with just a usename and password for apps like
+        facebook, instagram, and Google.
+      </p>
+      <Terminal userData={"passport.authenticate('facebook')"} selected="All" />
+      <p style={{ fontSize: 28}}>
+        Popular Strategies
+      </p>
+      <CardList />
+      <div style={{ marginBotton: 20}} />
+    </div>
+  );
+};
 
 export default Home;
