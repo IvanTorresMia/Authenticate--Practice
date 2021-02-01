c235cc537d1c2a26b51a98abf606fa00588d5326
Ivan Torres <ivantorresmia27@gmail.com>
2021-01-31 18:19:36 -0800

    using material ui and importing components

---
+++ src/Components/menus/UserDropDown.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import HomeIcon from '@material-ui/icons/Home';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+import ButtonList from '../Buttons/ButtonList'
+import history from '../../History'
+
+const UserDropDown = () => {
+
+    return (
+
+        <div>
+
+        </div>
+    )
+}
+
+export default UserDropDown;
