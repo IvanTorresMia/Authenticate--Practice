8052fa23ae6bb7c31c7ba5494028b3462242b286
Ivan Torres <ivantorresmia27@gmail.com>
2021-02-03 22:12:37 -0800

    creating user drop down and adding matrial ui styles

--- src/Components/menus/UserDropDown.jsx
+++ src/Components/menus/UserDropDown.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+
+// Material ui Comp
 import { withStyles } from '@material-ui/core/styles';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import Menu from '@material-ui/core/Menu';
@@ -8,17 +10,66 @@ import ListItemText from '@material-ui/core/ListItemText';
 import HomeIcon from '@material-ui/icons/Home';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 
+// Our Comp
 import ButtonList from '../Buttons/ButtonList'
 import history from '../../History'
+import IconButton from '../Buttons/IconButton';
+
+// Creating a raper component for other menu items. 
+const StyleMenu = withStyles( {
+    paper: {
+        border: "1px solid #d3d4d5"
+    }
+})(porps => {
+    <Menu 
+        elevation={0}
+        getContentAnchorEl={null}
+        anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "center"
+        }}
+        transformOrigin={{
+            vertical: "top",
+            horizontal: "center"
+        }}
+        {...props}
+    />
+});
+ 
+const StyleMenuItem = withStyles( theme => ({
+    root: {
+        "&:focus": {
+            backgroundColor: "vart(--primary-red)",
+            "& .MuiListItemIcon-root, & .MuiListItemText-primary": {
+                color: theme.palette.common.white,
+            }
+        }
+    }
+}))(MenuItem)
 
 const UserDropDown = () => {
+    const [anchorEl, SetAnchorEl] = React.useState(null)
+    const handleClick = (event) => {
+        SetAnchorEl(event.currentTrget);
+    }
+    const handleClose = () => {
+        SetAnchorEl(null)
+    }
+
 
     return (
-
-        <div>
+        <div
+            style={{ float: 'right'}}
+        >
+            <IconButton
+            href=""
+            style={{ color: 'white', padding: "13px 12px" }}
+            onClick={handleClick}
+            />
 
         </div>
     )
+
+
 }
 
-export default UserDropDown;
